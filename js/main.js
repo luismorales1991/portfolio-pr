32d1a8886de8295a5c86353561ecaa080ec9142f
lmorales2019047 <luisrodrigomorales6@gmail.com>
2022-03-02 11:59:15 -0600

    Se creo "about-area"

--- js/main.js
+++ js/main.js
@@ -22,4 +22,18 @@ $(document).ready(function() {
             enabled: true
         }
     });
+
+    $(".site-main .about-area .owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            544: {
+                items: 2
+            }
+        }
+    })
 })
